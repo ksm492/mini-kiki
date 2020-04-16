adb4fda4ea3d29edb88e771e10df0a12c29ac8bb
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-04-16 12:39:53 -0500

    added newtab override

--- background.js
+++ background.js
@@ -2,65 +2,65 @@
 // https://stackoverflow.com/questions/27239280/
 
 
-//Social Media Url Recognition
+//Social Media (02) Url Recognition
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     if (tab.url.indexOf("twitter") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     } else if (tab.url.indexOf("youtube") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     } else if (tab.url.indexOf("instagram") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     } else if (tab.url.indexOf("tiktok") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     } else if (tab.url.indexOf("netflix") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     } else if (tab.url.indexOf("disneyplus") != -1) {
-      chrome.tabs.create({url:"social.html"});
+      chrome.tabs.create({url:"02.html"});
     }
   }
 });
 
 
-//Shopping Url Recognition
+// Shopping (03) Url Recognition
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     if (tab.url.indexOf("amazon") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     } else if (tab.url.indexOf("urbanoutfitters") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     } else if (tab.url.indexOf("ae") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     } else if (tab.url.indexOf("ulta") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     } else if (tab.url.indexOf("sephora") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     } else if (tab.url.indexOf("brandymelville") != -1) {
-      chrome.tabs.create({url:"shopping.html"});
+      chrome.tabs.create({url:"03.html"});
     }
   }
 });
 
-//School Url Recognition
+//School (04) Url Recognition
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     if (tab.url.indexOf("drive.google") != -1) {
-      chrome.tabs.create({url:"school.html"});
+      chrome.tabs.create({url:"04.html"});
     } else if (tab.url.indexOf("mail.google") != -1) {
-      chrome.tabs.create({url:"school.html"});
+      chrome.tabs.create({url:"04.html"});
     } else if (tab.url.indexOf("newclasses") != -1) {
-      chrome.tabs.create({url:"school.html"});
+      chrome.tabs.create({url:"04.html"});
     } else if (tab.url.indexOf("zoom") != -1) {
-      chrome.tabs.create({url:"school.html"});
+      chrome.tabs.create({url:"04.html"});
     }
   }
 });
 
-//New Tab Url Recognition
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete') {
-    if (tab.url.indexOf("chrome://newtab/") != -1) {
-      chrome.tabs.create({url:"random.html"});
-    }
-  }
-});
+// //New Tab Url Recognition
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   if (changeInfo.status == 'complete') {
+//     if (tab.url.indexOf("chrome://newtab/") != -1) {
+//       chrome.tabs.create({url:"random.html"});
+//     }
+//   }
+// });
